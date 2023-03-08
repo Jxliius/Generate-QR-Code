9cc20cb9f4fde042520ccfb4cf09d50c76186636
Jxliius <julius.reiter001@gmail.com>
2023-03-08 12:31:26 +0100

    Create PasswordGenerator.py

---
+++ PasswordGenerator.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+def generate_password(length=8, include_digits=True, include_punctuation=True):
+    """Generates a random password of specified length."""
+    characters = string.ascii_letters
+    if include_digits:
+        characters += string.digits
+    if include_punctuation:
+        characters += string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+# Beispielaufruf
+password = generate_password(length=5, include_digits=True, include_punctuation=True)
+print(password)
